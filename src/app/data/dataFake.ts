169e31d0b2346e010c216ab79bf53888650b359e
Mirilice <mariaalicepco10@gmail.com>
2023-05-30 02:09:11 -0300

    first commit

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "Anel Coração Vermelho Elevado",
+    "description": "R$1060",
+    "photoCover":"https://pandorajoias.vtexassets.com/arquivos/ids/190265-800-auto?v=638094454761600000&width=800&height=auto&aspect=true"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Anel de Ouro Grande Coração ",
+    "description": "R$7450",
+    "photoCover":"https://pandorajoias.vtexassets.com/arquivos/ids/180962-800-auto?v=637274838231900000&width=800&height=auto&aspect=true"
+  },
+  {
+    "id":"3",
+    "title": "Anel Pandora Coração Brilhante",
+    "description": "R$1550",
+    "photoCover":"https://pandorajoias.vtexassets.com/arquivos/ids/187195-800-auto?v=637702584247830000&width=800&height=auto&aspect=true"
+  },
+  {
+    "id":"4",
+    "title": "Anel Rosetm Combinável Moldura Mini Coração",
+    "description": "R$820",
+    "photoCover":"https://pandorajoias.vtexassets.com/arquivos/ids/181224-800-auto?v=637280122296370000&width=800&height=auto&aspect=true"
   }
 ]
